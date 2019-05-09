42799ea3db5e85255aa206b9ebe743edad5255a4
Cian <111>
2019-05-09 14:31:56 +0800

    ProblemE 优化

--- src/ACM/ProblemE.java
+++ src/ACM/ProblemE.java
@@ -8,23 +8,26 @@ import java.util.Date;
 public class ProblemE {
 
     public static void main(String[] args) throws ParseException {
+        int EXAMPLE_NUMBER = 0;
+        String date_format = "MM/dd/yy";
+        String date_format1 = "yy/MM/dd";
 
+        ProblemE problemE = new ProblemE();
         Scanner scanner = new Scanner(System.in);
-        int EXAMPLE_NUMBER = Integer.parseInt(scanner.next());  //获取用例数量
 
-
-//        if (scanner.next() != null && scanner.next().matches("^[0-9]+$")){
-//            EXAMPLE_NUMBER = Integer.parseInt(scanner.next());
-//            if (EXAMPLE_NUMBER < 1 || EXAMPLE_NUMBER > 6 ){
-//                System.out.println("1≤T≤5!");
-//            }
-//        }else {
-//            System.out.println("please enter a number!1≤number≤5");
-//        }
-
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yy");   //设置输入时间格式
-        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("yy/MM/dd", Locale.ENGLISH);  //设置输入时间格式
+        if (scanner.hasNextInt()) {
+            int num = Integer.parseInt(scanner.next());
+            if (num > 0 && num < 5) {
+                EXAMPLE_NUMBER = num;
+                System.out.println(num);
+            } else {
+                System.out.println("请输入[1,5]之间的数字");
+            }
+        } else {
+            System.out.println("请输入数字");
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(date_format);   //设置输入时间格式
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat(date_format1, Locale.ENGLISH);  //设置输入时间格式
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM,yyyy", //设置输出时间格式
                 Locale.ENGLISH);
 
@@ -33,33 +36,69 @@ public class ProblemE {
 
         for (int i = 0; i < EXAMPLE_NUMBER; i++) {
             Scanner scanner1 = new Scanner(System.in);
+            String datee = scanner1.next();
 
-            str[i] = scanner1.next();
+            if ((   problemE.checkDateFormat(datee, date_format)
+                    ||
+                    problemE.checkDateFormat(datee, date_format1))) {
+                str[i] = datee;
+            } else {
+                System.out.println("日期格式不正确（19/05/09 or 05/09/19）");
+            }
         }
 
         for (int i = 0; i < EXAMPLE_NUMBER; i++) {
+            System.out.println(i);
 
             int b = Integer.parseInt(str[i].split("/")[0]);
+
             if (b > 12) {
 
                 date[i] = simpleDateFormat1.parse(str[i]);
                 System.out.println(sdf.format(date[i]));
             } else {
 
-                date[i] = simpleDateFormat.parse(str[i]);
-                Date date1 = simpleDateFormat1.parse(str[i]);
+                date[i] = simpleDateFormat.parse(str[i]);           //格式化为MM/dd/yy
+                Date date1 = simpleDateFormat1.parse(str[i]);       //格式化为yy/MM//dd
 
-                ProblemE problemE = new ProblemE();
-                long diff = date1.getTime() - date[i].getTime();
-                long days = problemE.abs(diff) / (1000 * 60 * 60 * 24);
+
+                long diff = date1.getTime() - date[i].getTime();        //相差毫秒数
+                long days = problemE.abs(diff) / (1000 * 60 * 60 * 24); //相差天数
 
                 System.out.println(days);
             }
         }
     }
 
+    /**
+     * 取绝对值
+     *
+     * @param a
+     * @return
+     */
     private long abs(long a) {
         return a > 0 ? a : -a;
+    }
+
+
+    /**
+     * 判断输入日期格式是否正确
+     *
+     * @param date
+     * @param format 目标格式
+     * @return
+     */
+    private boolean checkDateFormat(String date, String format) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+
+        simpleDateFormat.setLenient(false);
+
+        try {
+            simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
     }
 }
 
